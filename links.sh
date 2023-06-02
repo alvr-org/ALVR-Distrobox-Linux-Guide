1805304d10712fa95282792229f52548fc07de03
plyshka <leruop@gmail.com>
2023-06-02 13:26:51 +0500

    Initial commit - moved from another repository

---
+++ links.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ALVR_LINK='https://github.com/alvr-org/ALVR/releases/latest/download/ALVR-x86_64.AppImage'
+ALVR_FILENAME='ALVR-x86_64.AppImage'
+ALVR_APK_LINK='https://github.com/alvr-org/ALVR/releases/latest/download/alvr_client_android.apk'
+ALVR_APK_NAME='alvr_client_android.apk'
+WLXOVERLAY_LINK='https://github.com/galister/WlxOverlay/releases/download/v1.3.0/WlxOverlay-v1.3.0-x86_64.AppImage'
+WLXOVERLAY_FILENAME='WlxOverlay.AppImage'

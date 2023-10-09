8bce1f9b18d6218fb274c522a28740d931681ec7
plyshka <leruop@gmail.com>
2023-10-09 16:52:34 +0500

    Remove alvr appimage from steamvr processes

--- helper-functions.sh
+++ helper-functions.sh
@@ -4,7 +4,7 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-STEAMVR_PROCESSES=( vrdashboard vrcompositor vrserver vrmonitor vrwebhelper vrstartup ALVR-x86_64.AppImage)
+STEAMVR_PROCESSES=( vrdashboard vrcompositor vrserver vrmonitor vrwebhelper vrstartup )
 
 function echog() {
    echo -e "${RED}${STEP_INDEX}${NC} : ${GREEN}$1${NC}"

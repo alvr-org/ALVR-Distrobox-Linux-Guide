6c562d70f05a9418584a44be198cb3d9bf772378
plyshka <leruop@gmail.com>
2023-10-09 16:50:04 +0500

    Updated wlxoverlay links

--- links.sh
+++ links.sh
@@ -2,5 +2,5 @@
 
 ALVR_APK_LINK='https://github.com/alvr-org/ALVR/releases/latest/download/alvr_client_android.apk'
 ALVR_APK_NAME='alvr_client_android.apk'
-WLXOVERLAY_LINK='https://github.com/galister/WlxOverlay/releases/download/v1.3.1/WlxOverlay-v1.3.1-x86_64.AppImage'
+WLXOVERLAY_LINK='https://github.com/galister/WlxOverlay/releases/latest/download/WlxOverlay-v1.4.1-x86_64.AppImage'
 WLXOVERLAY_FILENAME='WlxOverlay.AppImage'

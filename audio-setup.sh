1958947bb4cd2526048242eab2bcfef871ecafb6
plyshka <leruop@gmail.com>
2023-11-15 22:36:17 +0500

    Fixed audio script crashing pipewire each disconnect

--- audio-setup.sh
+++ audio-setup.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-function get_alvr_playback_source_id() {
+function get_playback_sink_input_id() {
+  get_playback_id sink-inputs 'Sink Input' "$1"
+}
+
+function get_playback_source_output_id() {
+  get_playback_id source-outputs 'Source Output' "$1"
+}
+
+function get_playback_id() {
   local last_node_name=''
   local last_node_id=''
-  pactl list $1 | while read -r line; do
+  pactl list "$1" | while read -r line; do
     node_id=$(echo "$line" | grep -oP "$2 #\K.+" | sed -e 's/^[ \t]*//')
     node_name=$(echo "$line" | grep -oP 'node.name = "\K[^"]+' | sed -e 's/^[ \t]*//')
     if [[ "$node_id" != '' ]] && [[ "$last_node_id" != "$node_id" ]]; then
@@ -19,7 +27,7 @@ function get_alvr_playback_source_id() {
   done
 }
 
-function get_sink_id() {
+function get_sink_id_by_name() {
   local sink_name
   sink_name=$1
   pactl list short sinks | grep "$sink_name" | cut -d$'\t' -f1
@@ -34,37 +42,28 @@ function setup_mic() {
   # We link them together
   pw-link ALVR-MIC-Sink ALVR-MIC-Source
   # And we assign playback of pipewire alsa playback to created alvr sink
-  pactl move-sink-input "$(get_alvr_playback_source_id sink-inputs 'Sink Input' alsa_playback.vrserver)" "$(get_sink_id ALVR-MIC-Sink)"
+  pactl move-sink-input "$(get_playback_sink_input_id alsa_playback.vrserver)" "$(get_sink_id_by_name ALVR-MIC-Sink)"
+}
+
+function unload_modules() {
+  echo "Unloading audio, microphone sink & source"
+  pactl unload-module module-null-sink
 }
 
 function setup_audio() {
   echo "Setting up audio"
   pactl load-module module-null-sink sink_name=ALVR-AUDIO-Sink media.class=Audio/Sink
   pactl set-default-sink ALVR-AUDIO-Sink
-  pactl move-source-output "$(get_alvr_playback_source_id source-outputs 'Source Output' alsa_capture.vrserver)" "$(get_sink_id ALVR-AUDIO-Sink)"
-}
-
-function unload_mic() {
-  echo "Unloading microphone sink & source"
-  pw-cli destroy ALVR-MIC-Sink
-  pw-cli destroy ALVR-MIC-Source
-}
-
-function unload_sink() {
-  echo "Unloading audio sink"
-  pw-cli destroy ALVR-AUDIO-Sink
+  pactl move-source-output "$(get_playback_source_output_id alsa_capture.vrserver)" "$(get_sink_id_by_name ALVR-AUDIO-Sink)"
 }
 
 case $ACTION in
 connect)
-  unload_sink
-  unload_mic
-  sleep 1
+  unload_modules
   setup_mic
   setup_audio
   ;;
 disconnect)
-  unload_mic
-  unload_sink
+  unload_modules
   ;;
 esac

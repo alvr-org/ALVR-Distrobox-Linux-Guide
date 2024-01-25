6936653803ea21be32c5cc2b6202be26f05928fa
plyshka <leruop@gmail.com>
2024-01-26 03:39:23 +0500

    Revert "Completely redone script to use almost entirely pipewire utils and in python"
    This reverts commit d8f7837ce98eff24e4480ccdef31bf7e23f40984.

--- audio-setup.sh
+++ audio-setup.sh
@@ -1,117 +1,82 @@
-#!/usr/bin/python
-import os
-import subprocess
-import time
+#!/bin/bash
 
-use_headset_audio = True
-use_headset_mic = True
+# Set either to 0 to prevent from creating audio or microphone sinks and instead you using own headset or microphone
+USE_HEADSET_AUDIO=1
+USE_HEADSET_MIC=1
 
-temp_data_path = "/run/user/1000/alvr_audio"
+function get_playback_sink_input_id() {
+  get_playback_id sink-inputs 'Sink Input' "$1"
+}
 
+function get_playback_source_output_id() {
+  get_playback_id source-outputs 'Source Output' "$1"
+}
 
-def get_process_output(cmd: str):
-    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    data, err = process.communicate()
-    if process.returncode == 0:
-        return data.decode('utf-8').strip()
-    else:
-        print(err.decode('utf-8'))
-        exit(1)
+function get_playback_id() {
+  local last_node_name=''
+  local last_node_id=''
+  pactl list "$1" | while read -r line; do
+    node_id=$(echo "$line" | grep -oP "$2 #\K.+" | sed -e 's/^[ \t]*//')
+    node_name=$(echo "$line" | grep -oP 'node.name = "\K[^"]+' | sed -e 's/^[ \t]*//')
+    if [[ "$node_id" != '' ]] && [[ "$last_node_id" != "$node_id" ]]; then
+      last_node_id="$node_id"
+    fi
+    if [[ -n "$node_name" ]] && [[ "$last_node_name" != "$node_name" ]]; then
+      last_node_name="$node_name"
+      if [[ "$last_node_name" == "$3" ]]; then
+        echo "$last_node_id"
+        return
+      fi
+    fi
+  done
+}
 
+function get_sink_id_by_name() {
+  local sink_name
+  sink_name=$1
+  pactl list short sinks | grep "$sink_name" | cut -d$'\t' -f1
+}
 
-def unlink_node(node_name: str):
-    all_links = get_process_output("pw-link -l -I")
-    node_links = get_node_links(all_links, node_name)
-    for link in node_links:
-        subprocess.Popen(f"pw-link -d {link}", shell=True)
-
-
-def unlink_audio_record():
-    print("Unlinking previous nodes from audio record")
-    unlink_node("alsa_capture.vrserver")
-
-
-def unlink_mic_playback():
-    print("Unlinking previous nodes from mic playback")
-    unlink_node("alsa_playback.vrserver")
-
-
-def get_node_links(pw_output: str, node_name: str):
-    links = []
-    is_inside_links = False
-    for line in pw_output.split('\n'):
-        strip_line = line.strip()
-        if is_inside_links and not ("|<-" in strip_line or "|->" in strip_line):
-            is_inside_links = False
-        if not is_inside_links and node_name in strip_line and not ("|<-" in strip_line or "|->" in strip_line):
-            is_inside_links = True
-        if is_inside_links and ("|<-" in strip_line or "|->" in strip_line):
-            links.append(strip_line.split(' ')[0])
-    return links
-
-
-def unload_modules():
-    print('Unloading audio, microphone sink & source')
-    unlink_mic_playback()
-    unlink_audio_record()
-    subprocess.Popen(f'pw-cli destroy ALVR-AUDIO-Sink', shell=True)
-    subprocess.Popen(f'pw-cli destroy ALVR-MIC-Source', shell=True)
-
-
-def create_null_sink_with_name_and_class(name: str, class_name: str):
-    return subprocess.Popen("pw-cli create-node adapter "
-                            f"'{{ factory.name=support.null-audio-sink node.name={name} media.class={class_name} "
-                            "object.linger=true audio.position=[FL FR] monitor.channel-volumes=true }}'",
-                            shell=True)
-
-
-def setup_mic():
-    print('Creating microphone sink & source and linking alvr playback to it')
+function setup_mic() {
+  if [[ $USE_HEADSET_MIC == 1 ]]; then
+    echo "Creating microphone sink & source and linking alvr playback to it"
+    # This sink is required so that it persistently auto-connects to alvr playback later
+    pactl load-module module-null-sink sink_name=ALVR-MIC-Sink media.class=Audio/Sink | tee -a /run/user/1000/alvr-audio
     # This source is required so that any app can use it as microphone
-    create_null_sink_with_name_and_class("ALVR-MIC-Source", "Audio/Source/Virtual")
+    pactl load-module module-null-sink sink_name=ALVR-MIC-Source media.class=Audio/Source/Virtual | tee -a /run/user/1000/alvr-audio
+    # We link them together
+    pw-link ALVR-MIC-Sink:monitor_FL ALVR-MIC-Source:input_FL
+    pw-link ALVR-MIC-Sink:monitor_FR ALVR-MIC-Source:input_FR
+    # And we assign playback of pipewire alsa playback to created alvr sink
+    pactl move-sink-input "$(get_playback_sink_input_id alsa_playback.vrserver)" "$(get_sink_id_by_name ALVR-MIC-Sink)"
+    pactl set-default-source ALVR-MIC-Source
+  fi
+}
 
-    unlink_mic_playback()
+function unload_modules() {
+  echo "Unloading audio, microphone sink & source"
+  while read -r line; do
+    pactl unload-module "$line"
+  done <"/run/user/1000/alvr-audio"
+  >/run/user/1000/alvr-audio
+}
 
-    print("Linking microphone playback to microphone source")
-    subprocess.Popen("pw-link alsa_playback.vrserver:output_FL ALVR-MIC-Source:input_FL", shell=True)
-    subprocess.Popen("pw-link alsa_playback.vrserver:output_FR ALVR-MIC-Source:input_FR", shell=True)
+function setup_audio() {
+  if [[ $USE_HEADSET_AUDIO == 1 ]]; then
+    echo "Setting up audio"
+    pactl load-module module-null-sink sink_name=ALVR-AUDIO-Sink media.class=Audio/Sink | tee -a /run/user/1000/alvr-audio
+    pactl set-default-sink ALVR-AUDIO-Sink
+    pactl move-source-output "$(get_playback_source_output_id alsa_capture.vrserver)" "$(get_sink_id_by_name ALVR-AUDIO-Sink)"
+  fi
+}
 
-    print("Setting ALVR mic as default source")
-    subprocess.Popen("pactl set-default-source ALVR-MIC-Source", shell=True)
-
-
-def setup_audio():
-    print("Setting up audio")
-    create_null_sink_with_name_and_class("ALVR-AUDIO-Sink", "Audio/Sink")
-
-    unlink_audio_record()
-
-    print("Linking current nodes from to record")
-    subprocess.Popen("pw-link ALVR-AUDIO-Sink:monitor_FL alsa_capture.vrserver:input_FL", shell=True)
-    subprocess.Popen("pw-link ALVR-AUDIO-Sink:monitor_FR alsa_capture.vrserver:input_FR", shell=True)
-
-    print("Setting ALVR audio as default sink")
-    subprocess.Popen("pactl set-default-sink ALVR-AUDIO-Sink", shell=True)
-
-
-def do_action(action_name: str):
-    if action_name == 'connect':
-        unload_modules()
-        time.sleep(0.25)
-        if use_headset_audio:
-            setup_audio()
-        time.sleep(0.25)
-        if use_headset_mic:
-            setup_mic()
-        pass
-    elif action_name == 'disconnect':
-        unload_modules()
-        pass
-    else:
-        print('Unknown action')
-        pass
-
-
-if __name__ == "__main__":
-    action = os.environ.get('ACTION')
-    do_action(action)
+case $ACTION in
+connect)
+  unload_modules
+  setup_audio
+  setup_mic
+  ;;
+disconnect)
+  unload_modules
+  ;;
+esac

9b1acd9b0deb7b1f970e2dfddeb1755e98477eed
plyshka <leruop@gmail.com>
2024-03-11 21:04:48 +0500

    Add stop-container.sh

---
+++ stop-container.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cd $(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
+
+if [ "$EUID" -eq 0 ]; then
+   echo "Please don't run this script as root (no sudo)."
+   exit 1
+fi
+
+source ./env.sh
+source ./helper-functions.sh
+source ./setup-env.sh
+
+if [ "$(sanity_check_for_container)" -eq 1 ]; then
+   echor "There is more or less than 1 alvr container, something has gone wrong."
+   echor "Please report setup.log and list bellow to https://github.com/alvr-org/ALVR-Distrobox-Linux-Guide/issues"
+   distrobox list
+   exit 1
+fi
+
+distrobox stop "$container_name" --yes
